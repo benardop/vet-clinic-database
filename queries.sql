e13091bfbed8b4345717957f277880b851dae952
benardop <benard.oc.p@gmail.com>
2022-10-25 02:22:53 +0300

    Add queries.sql file

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 select * from animals where name like '%mon';
 select name from animals where (date_of_birth >= '2016-01-01' AND date_of_birth <= '2019-01-01');
- select name from animals where (neutered = 't' AND escape_attempts < 3);
+select name from animals where (neutered = 't' AND escape_attempts < 3);
 select date_of_birth from animals where (name like '%Agumon' OR name like '%Pikachu');
 select name, escape_attempts from animals where (weight_kg > 10.5);
 select * from animals where (neutered = 't');
