c686a30733c21c7ca70a5d504efce850ad841c8d
benardop <benard.oc.p@gmail.com>
2022-10-28 00:34:24 +0300

    Add schema.sql scripts

--- schema.sql
+++ schema.sql
@@ -5,7 +5,11 @@ CREATE TABLE animals(id serial PRIMARY KEY NOT NULL,name VARCHAR(50) NOT NULL,
     neutered BOOLEAN NOT NULL,weight_kg NUMERIC(5,2) NOT NULL
   );
 
-  /*Alter Table sql command*/
 
-  ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+  /*DAY-3 ALTER TABLES AND CREATE TABLE TASKS*/
+CREATE TABLE owners(id serial PRIMARY KEY, full_name VARCHAR(50) NOT NULL, age INT NOT NULL);
+CREATE TABLE species(id serial PRIMARY KEY, name VARCHAR(50) NOT NULL);
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN owners_id INT REFERENCES owners(id);
 
