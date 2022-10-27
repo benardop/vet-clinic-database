337b19a4a1077458b9d90bdf1914cc5cf67069f3
benardop <benard.oc.p@gmail.com>
2022-10-28 00:33:35 +0300

    Add data.sql scripts

--- data.sql
+++ data.sql
@@ -11,3 +11,15 @@ insert into animals(date_of_birth,escape_attempts,neutered,weight_kg,name) value
 insert into animals(date_of_birth,escape_attempts,neutered,weight_kg,name) values('06/07/2005', 7, true,20.4, 'Boarmon');
 insert into animals(date_of_birth,escape_attempts,neutered,weight_kg,name) values('10/13/1998', 3, true,17, 'Blossom');
 insert into animals(date_of_birth,escape_attempts,neutered,weight_kg,name) values('05/14/2022', 4, true, 22, 'Ditto');
+
+/*Day-3 Data insert query script*/
+
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 43),('Jennifer Orwell', 19),('Bob', 45),('Melody Pond', 77),('Dean Winchester', 14),('Jodie Whittaker',38);
+INSERT INTO species (name) VALUES ('Pokemon'),('Digimon');
+UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'Digimon') WHERE name LIKE '%mon';
+UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'Pokemon') WHERE species_id IS NULL;
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith') WHERE name = 'Agumon';
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell') WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob') WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond') WHERE name IN ('Charmander', 'Squirtle',  'Blossom');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester') WHERE name IN ('Angemon',  'Boarmon');
